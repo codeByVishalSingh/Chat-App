369a6b32b2e3928cece50700b8fb5ccc166953a9
Vishal Singh <vishalsingh123764@gmail.com>
2026-04-06 14:49:48 +0530

    adding chat section

--- client/src/Components/RightSidebar.jsx
+++ client/src/Components/RightSidebar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-const RightSidebar=() =>{
-    return (
+import assets from "../assets/assets";
+const RightSidebar=({selectedUser}) =>{
+    return  selectedUser && (
         <div> 
 
-        <h1> Porfile Page</h1>
+          <div>
+           <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" 
+           className="w-20 aspect-[1/1] rounded-full" />
+           <h1 className="px-10  text-x1 font-medium mx-auto flex items-center gap-2">
+            <p className="w-2 h-2 rounded-full bg-green-500 "></p>
+            {selectedUser.fullName}
+           </h1>
+           <p className="px-10 mx-auto">{selectedUser.bio}</p>
+          </div>
         </div>
     )
 }
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -4,15 +4,15 @@ import ChatContainer from "../Components/ChatContainer"
 import RightSidebar from "../Components/RightSidebar"
 const HomePage =() =>{
 
-const [selectedUser,setSelectedUser] = useState(false);
+const [selectedUser, setSelectedUser] = useState(false);
     return ( 
         <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'> 
 
         <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative 
-        ${selectedUser ? 'md:grid-cols-[1fr_1.5fr 1fr] xl:grid-cols-[1fr 2fr 1fr]' : 'md:grid-cols-2'}`}>
-            <Sidebar></Sidebar>
-            <ChatContainer></ChatContainer>
-            <RightSidebar></RightSidebar>
+        ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+            <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}></Sidebar>
+            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} ></ChatContainer>
+            <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}></RightSidebar>
         </div>
         </div>
     )
